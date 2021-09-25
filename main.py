d510ff1439c01dbed69c1a4e92854e6985588ae6
T-AbdelAlim <abdelalim.tareq@gmail.com>
2021-09-25 13:07:31 +0200

    test.py updated

--- main.py
+++ main.py
@@ -3,26 +3,21 @@ from harmonics.SurfaceLaplacian import MeshHarmonics
 
 
 if __name__ == '__main__':
-    print(f'\nMesh Processing Laplacian-Beltrami Operator ')
+    print(f'\nMesh Processing Laplace-Beltrami Operator ')
     print(f'=============================================')
-    cwd = os.getcwd().replace('\\', '/')
+    examples_path = os.getcwd().replace('\\', '/') + '/data/'
 
     eigenvectors=[10, 50, 75, 100, 150, 300]
 
-    M_ply = MeshHarmonics(cwd+'/data/ply_test.ply', n_vertices=1000)
-    # reconstruct meshes
-    M_ply.LBO_reconstruction(basis_functions=M_ply.basis_functions, EV_upper=25)
+    # calculate basis functions and natural frequencies:
+    M_ply = MeshHarmonics(examples_path+'/ply_test.ply', n_vertices=1000)
+
     # plot harmonics
     M_ply.plot_harmonics(EV_list=eigenvectors)
 
-    M_obj = MeshHarmonics(cwd+'/data/obj_test.obj', n_vertices=1000)
-    # reconstruct meshes
-    M_obj.LBO_reconstruction(basis_functions=M_obj.basis_functions, EV_upper=25)
-    # plot harmonics
-    M_obj.plot_harmonics(EV_list=eigenvectors)
+    # reconstruct mesh using the first e eigenvectors
+    for e in eigenvectors:
+        # reconstruct meshes
+        M_ply.LBO_reconstruction(basis_functions=M_ply.basis_functions, EV_upper=e)
 
-    M_stl = MeshHarmonics(cwd+'/data/stl_test.ply', n_vertices=1000)
-    # reconstruct meshes
-    M_stl.LBO_reconstruction(basis_functions=M_stl.basis_functions, EV_upper=25)
-    # plot harmonics
-    M_stl.plot_harmonics(EV_list=eigenvectors)
+
--- test.py
+++ test.py
@@ -3,14 +3,22 @@ from harmonics.SurfaceLaplacian import MeshHarmonics
 
 
 if __name__ == '__main__':
-    print(f'\nMesh Processing Laplacian-Beltrami Operator ')
+    print(f'\nMesh Processing Laplace-Beltrami Operator ')
     print(f'=============================================')
-    cwd = os.getcwd().replace('\\', '/')
+    examples_path = os.getcwd().replace('\\', '/') + '/examples/data/'
 
     eigenvectors=[10, 50, 75, 100, 150, 300]
 
-    M_ply = MeshHarmonics(cwd+'/examples/data/ply_bunny.ply', n_vertices=1000, examples=True)
-    # reconstruct meshes
-    M_ply.LBO_reconstruction(basis_functions=M_ply.basis_functions, EV_upper=20)
+    # calculate basis functions and natural frequencies:
+    M_ply = MeshHarmonics(examples_path+'/ply_bunny.ply', n_vertices=1000, examples=True)
+
     # plot harmonics
     M_ply.plot_harmonics(EV_list=eigenvectors)
+
+    # reconstruct mesh using the first e eigenvectors
+    for e in eigenvectors:
+        # reconstruct meshes
+        M_ply.LBO_reconstruction(basis_functions=M_ply.basis_functions, EV_upper=e)
+
+
+
